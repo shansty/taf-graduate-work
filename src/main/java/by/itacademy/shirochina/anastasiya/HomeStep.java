e089acb2a364106617211cdc970aae0f1faf00e3
Anastasiya Shirochina <shirochina16@gmail.com>
2023-06-22 18:56:53 +0300

    add Step and pom.xml

---
+++ src/main/java/by/itacademy/shirochina/anastasiya/HomeStep.java
@@ -0,0 +1,25 @@
+package by.itacademy.shirochina.anastasiya;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomeStep {
+    HomePage page;
+    public HomeStep (ChromeDriver driver, WebDriverWait wait){
+        page = new HomePage(driver, wait);
+    }
+    public void fillFormAndSubmit(String email, String password) {
+        page.openBaseURL();
+        page.waitForElementLocatedBy(page.closePopUpLocator);
+        page.closePopUp();
+        page.clickLoginButton();
+        page.waitElementLocatedBy(page.inputEmailLocator);
+        page.fieldEmail(email);
+        page.fieldPassword(password);
+        page.submitLoginForm();
+    }
+    public String getErrorMessage() {
+        page.waitElementLocatedBy(page.errorMessageLocator);
+        return page.getErrorMessage();
+    }
+}
