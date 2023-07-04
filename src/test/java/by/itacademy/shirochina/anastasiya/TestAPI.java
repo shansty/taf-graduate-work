15c22fde6b98ace742001dd629dc99070f8a9ed9
Anastasiya Shirochina <shirochina16@gmail.com>
2023-07-04 13:32:08 +0300

    refactor TestAPI class and pom.xml

--- src/test/java/by/itacademy/shirochina/anastasiya/TestAPI.java
+++ src/test/java/by/itacademy/shirochina/anastasiya/TestAPI.java
@@ -1,8 +1,7 @@
 package by.itacademy.shirochina.anastasiya;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.util.HashMap;
 
@@ -16,49 +15,30 @@ public class TestAPI {
         formParams.put("AUTH_FORM", "Y");
         formParams.put("TYPE", "AUTH");
         formParams.put("USER_LOGIN", "shirochina16@gmail.com");
-        formParams.put("USER_PASSWORD", "1234567");
+        formParams.put("USER_PASSWORD", "123456789");
         formParams.put("USER_REMEMBER", "Y");
-        given().formParams(formParams).when().post("https://markformelle.by/ajax/auth_ajax.php").then().log().body().assertThat().statusCode(200);
+        String htmlResponse = given().formParams(formParams).when().post("https://markformelle.by/ajax/auth_ajax.php").then().extract().asString();
+        Assertions.assertTrue(htmlResponse.contains("Вы успешно вошли на сайт!"));
     }
 
     @Test
-   public void submitLoginFormWithIncorrectData() {
-       WebDriver driver = new HtmlUnitDriver();
-
-        //try {
-            HashMap<String, String> formParams = new HashMap<>();
-            formParams.put("backurl", "/");
-            formParams.put("AUTH_FORM", "Y");
-            formParams.put("TYPE", "AUTH");
-            formParams.put("USER_LOGIN", "shirochina16@gmail.com");
-            formParams.put("USER_PASSWORD", "123");
-            formParams.put("USER_REMEMBER", "Y");
-            String htmlResponse = given().formParams(formParams).when().post("https://markformelle.by/ajax/auth_ajax.php").then().extract().asString();
-            System.out.println(htmlResponse);
-//            driver.get("data:text/html;charset=UTF-8," + htmlResponse);
-//            String xpathExpression = "//div/p/strong";
-//            WebElement element = driver.findElement(By.xpath(xpathExpression));
-//
-//            // Manipulate the selected element
-//            if (element != null) {
-//                // Print the text content of the element
-//                System.out.println("Text content: " + element.getText());
-//
-//                // Get the modified HTML code
-//                String modifiedHtml = driver.getPageSource();
-//                System.out.println("Modified HTML: " + modifiedHtml);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            // Quit the WebDriver instance
-//            driver.quit();
- //       }
+    public void submitLoginFormWithIncorrectData() {
+        HashMap<String, String> formParams = new HashMap<>();
+        formParams.put("backurl", "/");
+        formParams.put("AUTH_FORM", "Y");
+        formParams.put("TYPE", "AUTH");
+        formParams.put("USER_LOGIN", "shirochina16@gmail.com");
+        formParams.put("USER_PASSWORD", "123");
+        formParams.put("USER_REMEMBER", "Y");
+        String htmlResponse = given().formParams(formParams).when().post("https://markformelle.by/ajax/auth_ajax.php").then().extract().asString();
+        Assertions.assertTrue(htmlResponse.contains("Неверный Email или пароль."));
     }
+
     @Test
-   public void getMethodTest() {
-       HashMap<String, String> queryParams = new HashMap<>();
-       queryParams.put("q", "t-shirt");
-       given().queryParams(queryParams).when().get("https://markformelle.by/search").then().log().body();
+    public void getMethodTest() {
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("q", "t-shirt");
+        String htmlResponse = given().queryParams(queryParams).when().get("https://markformelle.by/search").then().extract().asString();
+        Assertions.assertTrue(htmlResponse.contains("Поиск"));
     }
 }
