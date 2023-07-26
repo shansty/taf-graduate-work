348049684172587f1da1dfd2b8aee42edb69a0c4
Anastasiya Shirochina <shirochina16@gmail.com>
2023-07-26 17:24:47 +0300

    delite Gson

--- src/test/java/by/itacademy/shirochina/anastasiya/api/TestAPI.java
+++ src/test/java/by/itacademy/shirochina/anastasiya/api/TestAPI.java
@@ -50,12 +50,14 @@ public class TestAPI {
         String actual = document.select("li[data-product-id]").select("input[value]").get(2).attr("value").toString();
         Assertions.assertEquals("Бюстгальтер t-shirt без косточек белого цвета", actual);
     }
+
     @Test
     public void searchForInvalidData() {
         String htmlResponse = given().queryParams(postObject.getQueryParams("lnl/cN?LAScn/lihACb?LBJ")).when().
-                get(postObject.searchEndpoint).then().assertThat().statusCode(200).extract().body().asString();;
+                get(postObject.searchEndpoint).then().assertThat().statusCode(200).extract().body().asString();
+        ;
         Document document = Jsoup.parse(htmlResponse);
-        String actual = document.select("div.text").text().toString();
+        String actual = document.select("div.text").text();
         Assertions.assertEquals("Сожалеем, но по вашему запросу ничего не найдено", actual);
     }
 }
